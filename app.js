c24ef9aac9eb86024b7cae8c0cd314d1acff4804
Daniel <hiroyusei@jhu.edu>
2017-01-23 02:51:49 +0800

    oops

--- app.js
+++
@@ -1,9 +0,0 @@
-/*Object literal:
-literals are fixed values, not variablesAn object literal is a list of zero or
-more pairs of property names and associated values of an object
-*/
-
-var student = {
-  name: "",
-  type: "student"
-};
